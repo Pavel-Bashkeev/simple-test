e31686c5694873e5cda629541ae36fa083f102f0
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-06-30 22:15:42 +0300

    осталось сделать вычисления

--- app/js/vendor/script.js
+++ app/js/vendor/script.js
@@ -2,6 +2,21 @@ const btn = document.querySelector('.btn');
 const modalCalc = document.querySelector('.modal');
 const calc = modalCalc.querySelector('.calc');
 const numeral = calc.querySelectorAll('.numeral');
+const result = document.querySelector('.result');
+
+numeral.forEach(item => {
+	item.addEventListener('click', () => {
+		if (item.getAttribute('data-value')) {
+			if (result.innerHTML == 0) {
+				result.innerHTML = '';
+			}
+			result.innerHTML += item.getAttribute('data-value');
+		} else {
+			result.innerHTML = 0;
+		}
+
+	})
+})
 
 
 
@@ -10,10 +25,13 @@ btn.addEventListener('click', modalOpened);
 modalCalc.addEventListener('click', function (e) {
 	let target = e.target;
 
-	if (!(target === calc && target === calc.children)) {
-		modalCalc.classList.remove('opened');
+	if (!(target === calc || target === calc.children[0] || target === calc.children[1])) {
+		console.log(target.classList.contains('numeral'))
+		if (!(target.classList.contains('numeral'))) {
+			document.body.classList.remove('scroll-hidden');
+			modalCalc.classList.remove('opened');
+		}
 	}
-
 })
 
 function modalOpened() {
