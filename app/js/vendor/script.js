444835a288e04071ea6d2603d23e0bf58d254695
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-06-30 18:41:57 +0300

    работа с логикой калькулятора

--- app/js/vendor/script.js
+++ app/js/vendor/script.js
@@ -1,2 +1,24 @@
-let a = 5;
-let b = 4;
+const btn = document.querySelector('.btn');
+const modalCalc = document.querySelector('.modal');
+const calc = modalCalc.querySelector('.calc');
+const numeral = calc.querySelectorAll('.numeral');
+
+
+
+btn.addEventListener('click', modalOpened);
+
+modalCalc.addEventListener('click', function (e) {
+	let target = e.target;
+
+	if (!(target === calc && target === calc.children)) {
+		modalCalc.classList.remove('opened');
+	}
+
+})
+
+function modalOpened() {
+	modalCalc.classList.add('opened');
+	if (modalCalc.classList.contains('opened')) {
+		document.body.classList.add('scroll-hidden');
+	}
+}
